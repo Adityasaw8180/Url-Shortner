57abffdf4fd745835ec45406c8efd0283030a407
Aditya Sawant <sawantaditya086@gmail.com>
2025-07-14 12:45:59 +0530

    router part of redirect url generated

--- routes/url.cjs
+++ routes/url.cjs
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { handleGenereateNewShortUrl } = require('../controllers/url.cjs')
+const { handleGenereateNewShortUrl,handleRedirectByShortId } = require('../controllers/url.cjs')
 router.get('/about', (req, res) => {
     res.send('This is about page');
 })
-    .post('/', handleGenereateNewShortUrl);
+    .post('/', handleGenereateNewShortUrl)
+    .get('/:shortId',handleRedirectByShortId)
 
 module.exports = router;
