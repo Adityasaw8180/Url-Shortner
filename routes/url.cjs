7fc694578ba51e5f00738a98715aad56cd6b461e
Aditya Sawant <sawantaditya086@gmail.com>
2025-07-14 17:06:15 +0530

    bug fixed

--- routes/url.cjs
+++ routes/url.cjs
@@ -2,17 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 const {
-  handleGenereateNewShortUrl,
-  handleRedirectByShortId,
-  handleAnalytics,
+    handleGenereateNewShortUrl,
+    handleRedirectByShortId,
+    handleAnalytics,
 } = require('../controllers/url.cjs');
 
 
 router.get('/about', (req, res) => {
-  res.send('This is about page');
+    res.send('This is about page');
 });
 
-router.post('/', handleGenereateNewShortUrl);
+// CORRECTED: Changed the POST route from '/' to '/url' to match frontend
+router.post('/url', handleGenereateNewShortUrl);
 
 router.get('/analytics/:shortId', handleAnalytics);
 
