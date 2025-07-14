7562d350e0c69f35832895ca711e0da0a85a7bcd
Aditya Sawant <sawantaditya086@gmail.com>
2025-07-14 14:20:54 +0530

    visited history analytics successesfull

--- routes/url.cjs
+++ routes/url.cjs
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { handleGenereateNewShortUrl,handleRedirectByShortId } = require('../controllers/url.cjs')
+const { handleGenereateNewShortUrl,handleRedirectByShortId,handleAnalytics } = require('../controllers/url.cjs')
 router.get('/about', (req, res) => {
     res.send('This is about page');
 })
     .post('/', handleGenereateNewShortUrl)
-    .get('/:shortId',handleRedirectByShortId)
+    .get('/analytics/:shortId',handleAnalytics)
+    .get('/:shortId',handleRedirectByShortId);
 
 module.exports = router;
